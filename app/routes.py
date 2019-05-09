93eca595607c3620f41f6d43205dfc83d4a416f8
Aelyce <38134449+Aelyce11@users.noreply.github.com>
2019-05-10 00:10:46 +0200

    fixed error 500 for some character sets

--- app/routes.py
+++ app/routes.py
@@ -45,22 +45,23 @@ def search():
           })
 
     if query.isnumeric():
-
-      for i in range(230000):
-        if str(query) in str(i):
-          res.append({
-                  'display': chr(i),
-                  'code': i,
-                  'name': unicodedata.name(chr(i), 'ANONYME')
-                })
+        for i in range(230000):
+          if query in str(i) and i not in range(55296, 57343):
+            res.append({
+                    'display': chr(i),
+                    'code': i,
+                    'name': unicodedata.name(chr(i), 'ANONYME')
+                  })
 
         # UnicodeEncodeError raised < 1000 : surrogates not allowed
         # try does not catch the error ?
-        # error comes from "query in str(i)"
+        # error comes from Cn character sets "High surrogates" and "High Private Use Surrogates"
+        # apparently these aren't valid unicode characters ?
+        # just gonna get rid of them until I find a better solution...
 
     if query[0: 2] == 'u+' or query[0: 2] == 'U+':
       for i in range(230000):
-        if ('U+%04x' % i) == unquote(query) or ('u+%04x' % i) == unquote(query):
+        if ('U+%04x' % i) == unquote(query) or ('u+%04x' % i) == unquote(query) and i not in range(55296, 57343):
           res.append({
             'display': chr(i),
             'code': i,
@@ -85,7 +86,8 @@ def uni(codepoint):
     'display': char,
     'name': unicodedata.name(char, 'ANONYME'),
     'decimal': int(codepoint),
-    'category': unicodedata.category(char)
+    'category': unicodedata.category(char),
+    'htmlentity': str(char.encode('ascii', 'xmlcharrefreplace'))[2:-1]
   }
 
   return render_template('unicode.html', random = rand, char = character)
